83883032c91c50be85d3431be6e60574ebc278c5
Prop-Art <91047507+Prop-Art@users.noreply.github.com>
2021-09-20 15:10:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-document.getElementById("albumimagei").src="assets/images/displaypanel/Slide1.JPG"
+document.getElementById("albumimagei").src="https://drive.google.com/file/d/1eoMAPcogPt2yCVRFPWrdr2Izq2iC7LAq/view"
 document.getElementById("albumimageii").src="assets/images/displaypanel/Slide2.JPG"
 document.getElementById("albumimageiii").src="assets/images/displaypanel/Slide3.JPG"
-
-
