2d9137faf362ed869ded7432deb393845fb85355
Prop-Art <91047507+Prop-Art@users.noreply.github.com>
2021-09-20 21:50:00 +0530

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("albumimagei").src="https://drive.google.com/file/d/1eoMAPcogPt2yCVRFPWrdr2Izq2iC7LAq/view"
-document.getElementById("albumimageii").src="assets/images/displaypanel/Slide2.JPG"
-document.getElementById("albumimageiii").src="assets/images/displaypanel/Slide3.JPG"
